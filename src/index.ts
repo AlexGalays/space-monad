260cfd748cca2af3e81b0ae87f43b63c6c590829
Rodrigo Silveira <r3850365@gmail.com>
2021-06-26 18:41:29 -0700

    Exported onemany in index.ts; added a few minor refinements.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { None, Some, Option } from './option'
-export { Err, Ok, Result } from './result'
-export { OneMany, One, Many } from './onemany'
+export { None, Some, Option } from './option';
+export { Err, Ok, Result } from './result';
+export { OneMany, One, Many } from './onemany';
